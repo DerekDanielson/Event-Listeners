c1e95bccd14aa65dcf6809608f122ea1c2c6a9b8
Derek <derekdanielson84@gmail.com>
2023-05-16 09:49:14 -0600

    Notes added

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ clearBtn.onclick = function(){
     alert('Clear Items');
 };
 
+// Add event listener
+clearBtn.addEventListener('click', function(){
+    alert('Clear Items');
+});
+// Or
+clearBtn.addEventListener('click', () => alert('Clear Items'));
+clearBtn.addEventListener('click', () => console.log('Clear Items'));
+clearBtn.addEventListener('click', onClear);
